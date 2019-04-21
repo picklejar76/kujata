cb32a5508779cbf95823dc5ef64f64fc65f2fd99
picklejar76 <picklejar76@gmail.com>
2019-04-21 18:04:28 -0500

    updated test-flevel-loader.js to use new config and generate useful metadata

--- ff7-asset-loader/test-flevel-loader.js
+++ ff7-asset-loader/test-flevel-loader.js
@@ -2,36 +2,82 @@ const fs = require("fs");
 const LzsDecompressor = require("../lzs/lzs-decompressor.js");
 const FLevelLoader = require("./flevel-loader.js");
 
-let config = {
-  "inputFLevelDirectory": "/opt/ff7/unpacked/field/flevel.lgp",
-  "outputFLevelDirectory": "../../kujata-data/data/field/flevel.lgp"
-};
+let config = JSON.parse(fs.readFileSync('../config.json', 'utf-8'));
 
 // Pre-requisite: Must run test-map-list-loader.json first to generate maplist.json
 // TODO: Make flevel-loader smarter so that it can do this automatically.
 
-let mapList = JSON.parse(fs.readFileSync(config.outputFLevelDirectory + '/maplist.json', 'utf-8'));
+let mapList = JSON.parse(fs.readFileSync(config.outputFieldFLevelDirectory + '/maplist.json', 'utf-8'));
 let lzsDecompressor = new LzsDecompressor();
 let flevelLoader = new FLevelLoader(lzsDecompressor, mapList);
 
 var replacer = function(k, v) {
-  if (k == "entitySections") { return undefined; }
+  ////if (k == "entitySections") { return undefined; }
   return v;
 };
 
 // translate just 1 map
-let flevelName = "md1stin"; // md1stin, md1_1, md1_2, nrthmk, junon
-let flevel = flevelLoader.loadFLevel(config, flevelName);
-fs.writeFileSync(config.outputFLevelDirectory + '/' + flevelName + '.json', JSON.stringify(flevel, replacer, 2));
+/*
+let fieldName = "ancnt1"; // md1stin, md1_1, md1_2, nrthmk, junon
+let flevel = flevelLoader.loadFLevel(config, fieldName);
+let outputFilename = config.outputFieldFLevelDirectory + '/' + fieldName + '.json';
+fs.writeFileSync(outputFilename, JSON.stringify(flevel, replacer, 2));
+console.log("Wrote: " + outputFilename);
+*/
 
-// translate all maps
+// translate all maps and create index
+let tempOpMetadata = {};
+
+let opCodeUsages = {};
 for (let fieldName of mapList) {
   if (fieldName && !fieldName.startsWith("wm")) {
     try {
       let flevel = flevelLoader.loadFLevel(config, fieldName);
-      fs.writeFileSync(config.outputFLevelDirectory + '/' + fieldName + '.json', JSON.stringify(flevel, replacer, 2));
+      let outputFilename = config.outputFieldFLevelDirectory + '/' + fieldName + '.json';
+      fs.writeFileSync(outputFilename, JSON.stringify(flevel, replacer, 2));
+      console.log("Wrote: " + fieldName + '.json');
+      for (let entity of flevel.script.entities) {
+        for (let script of entity.scripts) {
+          for (let i=0; i<script.ops.length; i++) {
+            let op = script.ops[i];
+            if (op && op.raw) {
+              let opHex = op.raw.substring(0, 2);
+              let usage = {
+                fieldName: fieldName,
+                entityName: entity.entityName,
+                scriptIndex: script.index,
+                opIndex: i
+              };
+              for (let opProperty of Object.keys(op)) {
+                usage[opProperty] = op[opProperty];
+              }
+              if (!opCodeUsages[opHex]) {
+                opCodeUsages[opHex] = [];
+              }
+              opCodeUsages[opHex].push(usage);
+              tempOpMetadata[opHex] = {
+                hex: opHex,
+                shortName: op.op
+              };
+            }
+          }
+        }
+      }
     } catch(e) {
-      console.error("Error while loading: " + fieldName, "" + e);
+      let errorMessage = "" + e;
+      if (errorMessage.includes("no such file or directory")) {
+        console.log("Warning: " + errorMessage);
+      } else {
+        console.error("Error while loading: " + fieldName, e);
+        process.exit();
+      }
     }
   }
 }
+for (let opHex of Object.keys(opCodeUsages)) {
+  let usages = opCodeUsages[opHex];
+  let filename = config.metadataDirectory + '/op-code-usages/' + opHex + '.json';
+  fs.writeFileSync(filename, JSON.stringify(usages, null, 2));
+  console.log("Wrote: " + filename);
+}
+console.log(JSON.stringify(tempOpMetadata, null, 2));
